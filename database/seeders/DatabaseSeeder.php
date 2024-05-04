f3068b42084511a1111a4df37d3a004c15dcd86c
Ranjeet <ranjeetyadav879@gmail.com>
2024-05-04 09:05:58 +0530

    DataBaseSeeder Changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
+         // \App\Models\User::factory(10)->create();
+
+         \App\Models\Role::create(['name'=>'Admin']);
+         \App\Models\Role::create(['name'=>'Author']);
+         \App\Models\Role::create(['name'=>'Editor']);
+         \App\Models\Role::create(['name'=>'Subscriber']);
+         
+
+
     }
 }
